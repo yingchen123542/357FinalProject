8a3291481371d0677b4c0e96274a470083e2a2e2
yingchen123542 <86224612+yingchen123542@users.noreply.github.com>
2022-05-02 14:50:41 -0700

    add initial files

---
+++ finalProject/Shared/finalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectApp.swift
+//  Shared
+//
+//  Created by yingying Chen on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
